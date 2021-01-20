e48850c15b790596b3142e8baea780940d2de303
Quent92 <77673311+Quent92@users.noreply.github.com>
2021-01-20 19:00:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Func = Callable[[Tensor], Tensor]
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-<<<<<<< HEAD
+
 
 def test_alizee():
     predicted = Tensor
