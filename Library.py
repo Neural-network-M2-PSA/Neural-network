61653446655ce727979aff44e1f8d785670f947c
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-20 14:07:56 +0100

    Création de la library
    Création de la library

---
+++ Library.py
@@ -0,0 +1,8 @@
+import numpy as np
+from typing import (Dict, Tuple, Callable,
+                    Sequence, Iterator, NamedTuple)
+from numpy import ndarray as Tensor
+
+Func = Callable[[Tensor], Tensor]
+
+
