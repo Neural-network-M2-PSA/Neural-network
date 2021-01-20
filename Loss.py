8b23c80f5accb1b3177a974ac5aabd19a785ffb9
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-20 16:09:54 +0100

    Loss function + mean square error

--- Loss.py
+++ Loss.py
@@ -6,8 +6,23 @@ from numpy import ndarray as Tensor
 Func = Callable[[Tensor], Tensor]
 
 class Loss:
+
     def loss(self, predicted: Tensor, actual: Tensor) -> float:
         raise NotImplementedError
 
     def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
         raise NotImplementedError
+
+
+class MeanSquareError(Loss):
+
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+
+
+        for i in range(len(predicted)):
+            mse = mse + (predicted[i] - actual[i]) ^ 2
+
+        return mse
+
+    # def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+    # return ...
--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-# This is a sample Python script.
+import numpy as np
+from typing import (Dict, Tuple, Callable,
+                    Sequence, Iterator, NamedTuple)
+from numpy import ndarray as Tensor
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+Func = Callable[[Tensor], Tensor]
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')
-    print(f'Hi, {name}')
-    print(f'Hi, {name}')
 
+def test_alizee():
+    predicted = Tensor
+    actual = Tensor
+    mse = MeanSquareError()
+    predicted = [1, 1, 1, 1, 1, 1]
+    actual = [1, 1, 2, 1, 3, 4]
+    mse.loss(predicted, actual)
+    print('loss:', mse)
+    return mse
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-def test_Leila() :
-    print('coucou')
+#def test_Leila() :
+    #print('coucou')
