8c853b0cb1caeb554fe997de7048902b0bd49b0d
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-20 14:43:11 +0100

    Classe loss 1

--- Library.py
+++ Library.py
@@ -5,4 +5,9 @@ from numpy import ndarray as Tensor
 
 Func = Callable[[Tensor], Tensor]
 
+class Loss:
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
 
+    def grad(self, predicted: Tensor, actual: Tensor) -> Tensor:
+        raise NotImplementedError
