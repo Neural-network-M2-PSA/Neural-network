bcbb1eddf9023a3ca25918dc14f36e39df71b699
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-21 16:12:08 +0100

    Class activation
    class Tanh(Activation) ne prend pas les argument tanh et tanh_prime

--- Layer.py
+++ Layer.py
@@ -82,3 +82,58 @@ class Linear(Layer):
          print("a : ", a)
          return a
 
+class Activation(Layer):
+    """
+    An activation layer just applies a function
+    element wise to its inputs
+    """
+    def __init__(self, f: Func, f_prime: Func) -> None:
+        super().__init__()
+        self.f = f
+        self.f_prime = f_prime
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        self.inputs = inputs
+        return self.f(inputs)
+
+    def backward(self, grad: Tensor) -> Tensor:
+        """
+        if y = f(x) and x = g(z)
+        then dy/dz = f'(x) * g'(z)
+        """
+        return self.f_prime(self.inputs) * grad
+
+
+    def tanh(x: Tensor) -> Tensor:
+        res = Tensor
+        res = np.tanh(x)
+        return res
+
+
+    def tanh_prime(x: Tensor) -> Tensor:
+        res = Tensor
+        res = 1 - np.tanh(x)**2
+        return res
+
+
+class Tanh(Activation):
+    def __init__(self):
+        super().__init__(tanh, tanh_prime)
+
+
+    def sigmoid(x: Tensor) -> Tensor:
+        res = Tensor
+        res = 0.5 + 0.5*np.tanh(x/2)
+        return res
+
+    """
+    def sigmoid_prime(x: Tensor) -> Tensor:
+        res = Tensor
+         
+        return res
+    """
+    """
+    class Sigmoid(Activation):
+        def __init__(self):
+            super().__init__(sigmoid, sigmoid_prime)
+    """
