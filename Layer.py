4097ad781acb79e436c456cc8628ba45dc91a9bf
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-28 12:06:24 +0100

    Linear Layer

--- Layer.py
+++ Layer.py
@@ -22,17 +22,18 @@ class Linear_layer(Layer):
     # input_size = number of input neurons
     # output_size = number of output neurons
     def __init__(self, input_size, output_size):
-        self.weights= np.random.rand(input_size, output_size) - 0.5
-        self.bias = np.random.rand(1, output_size) - 0.5
+        super().__init__()
+        self.weights = np.random.rand(input_size, output_size)
+        self.bias = np.random.rand(1, output_size)
 
     # returns output for a given input
-    def forward_propagation(self, input_data):
+    def forward(self, input_data : Tensor)-> Tensor:
         self.input = input_data
         self.output = np.dot(self.input, self.weights) + self.bias
         return self.output
 
     # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
-    def backward_propagation(self, output_error, learning_rate):
+    def backward(self, output_error, learning_rate) -> Tensor:
         input_error = np.dot(output_error, self.weights.T)
         weights_error = np.dot(self.input.T, output_error)
         # dBias = output_error
@@ -41,3 +42,4 @@ class Linear_layer(Layer):
         self.weights -= learning_rate * weights_error
         self.bias -= learning_rate * output_error
         return input_error
+
