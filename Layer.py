6f1bab562d4b63ba265fc6bf7d35354a1a3f7c7d
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-01-28 12:08:08 +0100

    Activation_layer

--- Layer.py
+++ Layer.py
@@ -43,3 +43,19 @@ class Linear_layer(Layer):
         self.bias -= learning_rate * output_error
         return input_error
 
+class Activation_layer(Layer):
+    def __init__(self, activation, activation_prime):
+        super().__init__()
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    # returns the activated input
+    def forward(self, input_data):
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    # Returns input_error=dE/dX for a given output_error=dE/dY.
+    # learning_rate is not used because there is no "learnable" parameters.
+    def backward(self, output_error, learning_rate):
+        return self.activation_prime(self.input) * output_error
