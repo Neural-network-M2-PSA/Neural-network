e31aa43104c481350b221869433f0c3f7eb236c3
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-02-02 11:13:15 +0100

    layer2

---
+++ layer2.py
@@ -0,0 +1,59 @@
+import numpy as np
+from numpy import ndarray as Tensor
+
+
+
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.output = None
+
+    # computes the output Y of a layer for a given input X
+    def forward(self, inputs: Tensor) -> Tensor:
+        raise NotImplementedError
+
+    # computes dE/dX for a given dE/dY (and update parameters if any)
+    def backward(self, output_error, learning_rate) -> Tensor:
+        raise NotImplementedError
+
+class Linear_layer(Layer):
+    # input_size = number of input neurons
+    # output_size = number of output neurons
+    def __init__(self, input_size, output_size):
+        super().__init__()
+        self.weights = np.random.rand(input_size, output_size)
+        self.bias = np.random.rand(1, output_size)
+
+    # returns output for a given input
+    def forward(self, input_data : Tensor)-> Tensor:
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+        return self.output
+
+    # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
+    def backward(self, output_error, learning_rate) -> Tensor:
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error.T)
+        # dBias = output_error
+
+        # update parameters
+        self.weights -= learning_rate * weights_error
+        self.bias -= learning_rate * output_error
+        return input_error
+
+class Activation_layer(Layer):
+    def __init__(self, activation, activation_prime):
+        super().__init__()
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    # returns the activated input
+    def forward(self, input_data):
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    # Returns input_error=dE/dX for a given output_error=dE/dY.
+    # learning_rate is not used because there is no "learnable" parameters.
+    def backward(self, output_error, learning_rate):
+        return self.activation_prime(self.input) * output_error
