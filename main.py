9c6aa4003e99c87b91a15c6bd6ad3b220e5d15f5
Alizee12 <67470838+Alizee12@users.noreply.github.com>
2021-02-03 17:23:48 +0100

    test avec 4 variables

--- main.py
+++ main.py
@@ -6,30 +6,35 @@ from Loss2 import mse, mse_prime
 
 import pandas as pd
 
-data = pd.read_csv('D:\dataset.csv')
+data = pd.read_csv('D:\data_train.csv')
+data_test = pd.read_csv('D:\data_train.csv')
 #print(data.head())
-set_size = 100
+set_size = 258
 
 # training data
-x_train = np.array(data[['cosTBz','R2','chiProb']][0:set_size])
+x_train = np.array(data[['chiProb','Ks_distChi','m_KSpipi_norm','Mbc_norm']][0:set_size])
 y_train = np.array(data[['isSignal']][0:set_size])
+
 
 # network
 net = Network()
-net.add(Linear_layer(3, 3))
+
+net.add(Linear_layer(4, 4))
 net.add(Activation_layer(tanh, tanh_prime))
-#net.add(Activation_layer(relu, relu_prime))
-net.add(Linear_layer(3, 1))
-net.add(Activation_layer(tanh, tanh_prime))
-#net.add(Activation_layer(relu, relu_prime))
+
+net.add(Activation_layer(sigmoid, sigmoid_prime))
+net.add(Linear_layer(4, 1))
+
 
 
 # train
 net.use(mse, mse_prime)
-net.fit(x_train, y_train, epochs=300, learning_rate=0.1)
+net.fit(x_train, y_train, epochs=258, learning_rate=0.01)
 
 # test
-out = net.predict(x_train)
+x_test = np.array(data_test[['chiProb','Ks_distChi','m_KSpipi_norm','Mbc_norm']][0:set_size])
+y_test = np.array(data_test[['isSignal']][0:set_size])
+out = net.predict(x_test)
 #print(out)
 #print("Type d'entrée", type(x_train))
 #print("Type de sortie", type(out))
@@ -39,22 +44,30 @@ out = net.predict(x_train)
 print("shape de sortie", out.shape)
 print("shape y_train", y_train.shape)
 
-out2 = net.predict(x_train)
-#for i in range(301):
-    #out2[0][i] = round(out[0][i])
 
+nb_true=0
+nb_false=0
 
+min = 0
+tmp = 10
+for i in range(258):
 
-
-
-
-for i in range(100):
+    #print("____________________________________________________")
+    #print("step: ", i)
+    #print("Prédiction: ", round(out[i][0]))
+    #print("Valeur réelle: ", round(y_test[i][0]))
+    if(round(out[i][0]) == round(y_test[i][0])):
+        nb_true=nb_true+1
+        print("TRUE")
+    if (round(out[i][0]) != round(y_test[i][0])):
+        print("FALSE")
+        nb_false=nb_false+1
     print("____________________________________________________")
-    print("step: ", i)
-    print("Prédiction: ", round(out[i][0]))
-    print("Valeur réelle: ", y_train[i][0])
-    print("____________________________________________________")
 
+
+print("nb_true", nb_true)
+print("nb_false", nb_false)
+print("Pourcentage juste: ", nb_true/(nb_true+nb_false)*100, "%")
 
 #print(y_train)
 
