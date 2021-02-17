382a3433b40e47446c398da06b902086131d86ad
LeilaGodinaud <77676504+LeilaGodinaud@users.noreply.github.com>
2021-02-17 15:30:10 +0100

    Update error_round.py
    fonction erreur avec juste arrondi à l'unité

--- Neural_Network_Library/error_round.py
+++ Neural_Network_Library/error_round.py
@@ -9,13 +9,14 @@ GODINAUD Leila, leila.godinaud@gmail.com
 
 """_______error_round_____"""
 '''
-This function return the percentage of true answer within 0.4
+This function returns the percentage of false answers with a round of the prediction to the unit
 '''
 
 # Library
 import numpy as np
 
-def error_round(y_prediction, y_actual):
-    diff = y_prediction - y_actual
-    diff_true = diff[np.abs(diff) <= 0.4 ]
-    return 100 - diff_true.size*100/diff.size
+def error_round(y_prediction, y_target):
+    y_prediction = np.round(y_prediction)
+    y_target = np.round(y_target)
+    nbr_error = y_target[y_prediction != y_target].size
+    return nbr_error* 100/y_target.size
